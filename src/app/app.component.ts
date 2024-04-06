03d762886e764e11a00c06427b18d5d79c1077a8
AbdullohDev07 <gamesupro01@gmail.com>
2024-04-06 15:12:13 +0500

    push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'leetcodeExampleTwo';
+  
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
